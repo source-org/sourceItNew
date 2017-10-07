5b7f25497386b6bfb8911e19f4f465361622ae36
alexshimel91 <alexshimel91@gmail.com>
2017-10-08 00:03:51 +0300

    InvalidParameterException doesn't compile in ListContestTest

---
+++ src/homework7/Demo.java
@@ -0,0 +1,26 @@
+package homework7;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Random;
+
+public class Demo {
+    public static void main(String[] args) {
+
+            ArrayList<Integer> arrayList = new ArrayList<Integer>();
+            LinkedList<Integer> linkedList = new LinkedList<Integer>();
+            for(int i = 0; i < 1000000; i++) {
+                Integer variable = new Random().nextInt(Integer.MAX_VALUE);
+                arrayList.add(variable);
+                linkedList.add(variable);
+            }
+            Operations listOperations = new ListContest();
+            Long startOperation = System.currentTimeMillis();
+            listOperations.arrayListOperations(arrayList);
+            System.out.println("List operation with ArrayList took: " + (System.currentTimeMillis() - startOperation) + " milliseconds");
+            startOperation = System.currentTimeMillis();
+            listOperations.linkedListOperations(linkedList);
+            System.out.println("List operation with LinkedList took: " + (System.currentTimeMillis() - startOperation) + " milliseconds");
+
+    }
+}
---
+++ src/homework7/Operations.java
@@ -0,0 +1,9 @@
+package homework7;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public interface Operations {
+    <T> ArrayList<T> arrayListOperations(ArrayList<T> arrayList);
+    <T> LinkedList<T> linkedListOperations(LinkedList<T> linkedList);
+}
