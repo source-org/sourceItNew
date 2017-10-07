7e1fd88a10fa502b216525098460df8d50852efe
alexshimel91 <alexshimel91@gmail.com>
2017-10-07 12:40:10 +0300

    UpdateUtil

--- src/homework6/util/Util.java
+++ src/homework6/util/Util.java
@@ -5,11 +5,29 @@ import homework6.IllegalResourceException;
 public class Util {
 
     public static <T> T arrayIndexSearcher(T[] array, int index) throws IndexOutOfBoundsException,IllegalArgumentException {
+        try {
+        return array[index];
+        } catch (IndexOutOfBoundsException iob) {
+            iob.getMessage();
+        } catch (IllegalArgumentException iae) {
+            iae.getStackTrace();
+        }
         return array[index];
     }
 
     public int castMethod (String phrase) throws ClassCastException, NullPointerException, NumberFormatException{
-        return Integer.valueOf(phrase);
+        Integer result = null;
+        try {
+            if (phrase == null) {
+                throw new NullPointerException();
+            }
+          result = Integer.valueOf(phrase);
+        } catch (ClassCastException | NumberFormatException ce) {
+            ce.printStackTrace();
+        } catch (NullPointerException ne) {
+            ne.printStackTrace();
+        }
+        return result;
     }
     public double multiCast (Object object) throws ClassCastException, NullPointerException, NumberFormatException{
         String firstCast = object.toString();
